0339c97c1fc6da1a52039fd5e9496b699dddd4d1
Mark van Booren <markvanbooren@hotmail>
2021-02-17 10:14:21 +0100

    new branch

---
+++ script.js
@@ -0,0 +1,48 @@
+
+// const biggerThanhundred = function(item) {
+
+//     const item2 = item +item;
+ 
+// if (item2 > 100) {
+//     console.log( item2)
+//     return;}
+   
+
+// console.log('to low..............................')
+// };
+
+
+// biggerThanhundred(20);
+
+
+// const bouncerBrenda = function (age, crowdAttheMoment){
+
+// if(age<=17){
+//     console.log('U r to young...');
+//  return ;
+// };
+// if(crowdAttheMoment>1000){
+//     console.log("come back later...")
+//     return;
+// };
+//     if( age>=18 ){
+//         console.log( "come in");
+//     };
+ 
+// }
+// bouncerBrenda(30, 899)
+// bouncerBrenda(99, 1999)
+// bouncerBrenda(3, 1290)
+// bouncerBrenda(18, 1000)
+// bouncerBrenda(18, 999)
+// bouncerBrenda(17, 999)
+
+const averageOfFIve = function (num1,num2,num3,num4,num5){
+const average = (num1+num2+num3+num4+num5)/5
+
+console.log(average);
+return "hello";
+
+}
+
+averageOfFIve(1,2,3,4,5)
